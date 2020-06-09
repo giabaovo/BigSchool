d2704acaed15dc324b0e97a6c92c5c6e7b56fca3
Administrator <Administrator@PC225>
2020-06-09 11:27:53 +0700

    BaoNgu

--- bigschool/ViewModels/CourseViewModel.cs
+++ bigschool/ViewModels/CourseViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using bigschool.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace bigschool.ViewModels
 {
-    public class CourseViewModel
+    public class CourseViewModel 
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
--- bigschool/ViewModels/ValidTime.cs
+++ bigschool/ViewModels/ValidTime.cs
@@ -7,16 +7,16 @@ using System.Web;
 
 namespace bigschool.ViewModels
 {
-    public class FutureDate : ValidationAttribute
+    public class ValidTime : ValidationAttribute
     {
         public override bool IsValid(object value)
         {
             DateTime dateTime;
-            var isValid = DateTime.TryParseExact(Convert.ToString(value), "dd/M/yyyy",
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "HH:mm",
                 CultureInfo.CurrentCulture,
                 DateTimeStyles.None,
                 out dateTime);
-            return (isValid && dateTime > DateTime.Now);
+            return isValid;
         }
     }
 }
